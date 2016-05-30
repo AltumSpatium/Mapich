6a44b7fc9a9ccb66c26b737dc27251e598fee3d3
Alex Martynov <sinner.in.me11@gmail.com>
2016-05-30 19:09:58 +0200

    See desrciption
    Added two pages for settings and displaying result
    Links are works correctly now
    Added some views to display pages
    Data from DB now can be displayed

--- search/urls.py
+++ search/urls.py
@@ -3,4 +3,7 @@ import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
+	url(r'^index.html$', views.index, name='index'),
+	url(r'^results.html$', views.results, name='results'),
+	url(r'^settings.html$', views.settings, name='settings'),
 ]
--- search/views.py
+++ search/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from models import SearchResult
 
 def index(request):
 	return render(request, 'index.html', {})
+
+def results(request):
+	results = SearchResult.objects.all()
+	return render(request, 'results.html', {'results': results})
+
+def settings(request):
+	return render(request, 'settings.html', {})
