6ee8bc0423420503f9754558d95f5916d52af88c
Mikhail_Lyapich <evildevil676@gmail.com>
2016-06-05 17:07:00 +0300

    Crawler v1.1
    Added indexing, stemming, checking stopwords and robots.txt

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -3,6 +3,11 @@ import re
 from bs4 import BeautifulSoup
 import time
 import urlparse
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+import robotparser
+
+cached_stopwords = set(stopwords.words('english'))
 
 
 def visible(element):
@@ -23,10 +28,38 @@ class Crawler:
         self.depth_links = []
         self.sites = []
         self.visited = set()
+        self.index = {}
+        self.inverted_index = {}
+
+    def get_domain(self, link):
+        parsed_url = urlparse.urlparse(link)
+        domain = '{url.scheme}://{url.netloc}/'.format(url=parsed_url)
+        return domain
+
+    def parsing_robots(self, link):
+        domain = self.get_domain(link)
+        parser = robotparser.RobotFileParser()
+        parser.set_url(urlparse.urljoin(domain, 'robots.txt'))
+        parser.read()
+        return parser
+
+    def get_site(self, link):
+        links = []
+        start_page = requests.get(link)
+        soup = BeautifulSoup(start_page.text, 'lxml')
+        text = filter(visible, soup.findAll(text=True))
+        parser = self.parsing_robots(link)
+        for tag in soup.find_all('a', href=True):
+            new_link = urlparse.urljoin(link, tag['href'])
+            if new_link not in self.visited and parser.can_fetch('*', new_link):
+                links.append(new_link)
+                self.visited.add(new_link)
+        return Site(link, text, links)
 
     def crawl(self):
         site = self.get_site(self.starting_url)
-        self.sites.append(site)
+        self.sites.append(site)  # maybe we don't need to save our sites
+        self.index[site.name] = site.index()
         self.depth_links.append(site.links)
         for current_depth in xrange(self.depth):
             current_links = []
@@ -34,47 +67,59 @@ class Crawler:
                 current_site = self.get_site(link)
                 current_links.extend(current_site.links)
                 self.sites.append(current_site)
+                self.index[current_site.name] = current_site.index()
+                print self.index
                 # time.sleep(1)
             self.depth_links.append(current_links)
+        print self.invert_index(self.index)
 
-
-    def get_site(self, link):
-        links = []
-        start_page = requests.get(link)
-        soup = BeautifulSoup(start_page.text, 'lxml')
-        text = filter(visible, soup.findAll(text=True))
-        self.get_words(text)
-        # indexing_texts(text,link)
-        print text
-        for tag in soup.find_all('a', href=True):
-            new_link = urlparse.urljoin(link, tag['href'])
-            if new_link not in self.visited:
-                links.append(new_link)
-                self.visited.add(new_link)
-        # print links
-        # print self.visited
-        return Site(text, links)
-
-    def get_words(self, texts):
-        words = []
-        pattern = re.compile('[\W_]+')
-        for t in texts:
-            t = t.lower()
-            t = pattern.sub(' ', t)
-            re.sub(r'[\W_]+','', t)
-            words.extend(t.split())
-        print words
-
+    def invert_index(self, index):
+        inverted_index = {}
+        for site_name in index.keys():
+            for word in index[site_name].keys():
+                if word in inverted_index.keys():
+                    if site_name in inverted_index[word].keys():
+                        inverted_index[word][site_name].extend(
+                            index[site_name][word][:])
+                    else:
+                        inverted_index[word][
+                            site_name] = index[site_name][word]
+                else:
+                    inverted_index[word] = {site_name: index[site_name][word]}
+        return inverted_index
 
 
 class Site(object):
     """docstring for Site"""
 
-    def __init__(self, text, links):
+    def __init__(self, name, text, links):
+        self.name = name
         self.text = text
         self.links = links
 
+    def get_words(self):
+        stemmer = PorterStemmer()
+        words = []
+        pattern = re.compile('[\W_]+')
+        for t in self.text:
+            t = t.lower()
+            t = pattern.sub(' ', t)
+            re.sub(r'[\W_]+', '', t)
+            words.extend(t.split())
+        result = [stemmer.stem(word)
+                  for word in words if word not in cached_stopwords]
+        return result
+
+    def index(self):
+        termlist = self.get_words()
+        word_index = {}
+        for index, word in enumerate(termlist):
+            try:
+                word_index[word].append(index)
+            except:
+                word_index[word] = [index]
+        return word_index
 
 c = Crawler(
-    'http://vitals.lifehacker.com/what-s-the-difference-between-all-these-running-apps-1779631255', 0)
+    'https://pymotw.com/2/robotparser/', 1)
 c.crawl()
