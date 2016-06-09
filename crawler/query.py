abed3521f75b6431128471266a50723d911e0c09
Mikhail_Lyapich <evildevil676@gmail.com>
2016-06-09 20:52:33 +0300

    I'm really stupid

--- crawler/query.py
+++ crawler/query.py
@@ -37,7 +37,7 @@ def word_query(word):
 
 def free_query(query):
     res = []
-    query = db.get_word_data(query)
+    query = get_words(query)
     for word in query:
         res += word_query(word)
     print res
